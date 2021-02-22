66e58b2fe492a19d8c338f9d8c69cf6cca77f606
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-02-22 22:03:25 +0100

    Display order supbage as default

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,34 @@
-import {settings, select} from './settings.js';
+import {settings, select, classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = 
   {
-    init: function()
-    {
+    initPages: function(){
       const thisApp = this;
-    
-      thisApp.initData();
-      thisApp.initCart();
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      thisApp.activatePage(thisApp.pages[0].id);
     },
 
-    initData: function() 
-    {
+    activatePage: function(pageId){
       const thisApp = this;
 
-      thisApp.data = {};
+      /* add class activ to maching apges , remove from non matching*/
+      for(let page of thisApp.pages){
+        page.classList.toggle(classNames.pages.active, page.id == pageId);
+      }
 
-      const url = settings.db.url + '/' + settings.db.product;
-
-      fetch(url)
-        .then(function(rawResponse){
-          return rawResponse.json();
-        })
-        .then(function(parsedResponse){
-
-          thisApp.data.products = parsedResponse;
-
-          thisApp.initMenu();
-
-        });
+      /* add class activ to maching links , remove from non matching*/
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(
+          classNames.nav.active, 
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
     },
 
     initMenu: function()
@@ -56,6 +54,37 @@ const app =
 
         app.cart.add(event.detail.product);
       });
+    },
+
+    initData: function() 
+    {
+      const thisApp = this;
+
+      thisApp.data = {};
+
+      const url = settings.db.url + '/' + settings.db.product;
+
+      fetch(url)
+        .then(function(rawResponse){
+          return rawResponse.json();
+        })
+        .then(function(parsedResponse){
+
+          thisApp.data.products = parsedResponse;
+
+          thisApp.initMenu();
+
+        });
+    },
+
+    init: function()
+    {
+      const thisApp = this;
+      
+      thisApp.initPages();
+
+      thisApp.initData();
+      thisApp.initCart();
     },
   };
 
