6ede7882890b9a2de52b7b2595b6635a252c00ac
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-02-24 17:23:00 +0100

    Working with subpages

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,33 @@ const app =
 
       thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
-      thisApp.activatePage(thisApp.pages[0].id);
+
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      let pageMatchingHash = thisApp.pages[0].id;
+
+      for(let page of thisApp.pages){
+        if(page.id == idFromHash){
+          pageMatchingHash = page.id;
+          break;
+        }   
+      }
+
+      thisApp.activatePage(pageMatchingHash);
+
+      for (let link of thisApp.navLinks)
+      {
+        link.addEventListener('click', function(event){
+          const clickedElement = this;
+
+          event.preventDefault();
+
+          const id = clickedElement.getAttribute('href').replace('#', '');
+          thisApp.activatePage(id);
+
+          window.location.hash = '#/' + id;
+        }); 
+      }
     },
 
     activatePage: function(pageId){
