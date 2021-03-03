52b27fe607c241bfb222e70e93cff89cf2387586
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-03-03 17:59:38 +0100

    Finish BaseWidget class

--- src/js/components/BaseWidget.js
+++ src/js/components/BaseWidget.js
@@ -5,17 +5,23 @@ class BaseWidget{
     thisWidget.dom = {};
     thisWidget.dom.wrapper = wrapperElement;
 
-    thisWidget.value = initialValue;
+    thisWidget.correctValue = initialValue;
   }
 
-  setValue(value){
+  get value (){
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+
+  set value(value){
     const thisWidget = this;
         
     const newValue = thisWidget.parseValue(value);
     
-    if(newValue !== thisWidget.value && thisWidget.isValid(newValue))
+    if(newValue !== thisWidget.correctValue && thisWidget.isValid(newValue))
     {
-      thisWidget.value = newValue;
+      thisWidget.correctValue = newValue;
       thisWidget.announce();
     }
     
@@ -45,6 +51,12 @@ class BaseWidget{
     });
 
     thisWidget.dom.wrapper.dispatchEvent(event);
+  }
+
+  setValue(value) {
+    const thisWidget = this;
+
+    thisWidget.value= value;
   }
 }
 
